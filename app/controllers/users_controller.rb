62a78df3c3eeaf53b2cddd0932810052ca85d8d1
takehanKosuke <kounotakashiyuu@kosukeKawano.local>
2018-04-29 14:39:50 +0900

    Make logout function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,14 @@
 class UsersController < ApplicationController
-   before_action :sign_in?
+   before_action :sign_in?, only: :show
 
   def show
+   # @user = User.find(params[:id])
   end
-
 
   private
   def sign_in?
-    user_signed_in?
+    unless user_signed_in?
       redirect_to "/users/sign_in", method: :get, alert:"ログインして下さい"
+    end
   end
-  # def show
-  #   @user = User.find(params[:id])
-  # end
 end
