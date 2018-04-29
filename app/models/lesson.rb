fad53309cc43cf937c40bc881c8d0cd8969a6dee
takehanKosuke <kounotakashiyuu@kosukeKawano.local>
2018-04-29 13:24:42 +0900

    Make  Intermediate table association

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,6 @@
 class Lesson < ActiveRecord::Base
   belongs_to  :term
   belongs_to  :department
+
+  has_many    :lessons, through: :users_lessons
 end
--- app/models/users_lesson.rb
+++ app/models/users_lesson.rb
@@ -1,2 +1,6 @@
 class UsersLesson < ActiveRecord::Base
+  belongs_to  :user
+  belongs_to  :lesson
+
+  has_many    :users, through: :users_lessons
 end
