69eaa783ea918f35c65d0a3542e6527384a5b9f0
takehanKosuke <kkoussuke530@icloud.com>
2018-04-30 21:02:16 +0900

    make department create form

--- app/controllers/departmants_controller.rb
+++ app/controllers/departmants_controller.rb
@@ -2,4 +2,15 @@ class DepartmantsController < ApplicationController
   def new
     @user = User.find(current_user.id)
   end
+
+  def create
+    Department.create(faculty: departmants_params[:faculty],depat_cord: departmants_params[:depat_cord])
+    redirect_to "/"
+  end
+
+
+  private
+  def departmants_params
+    params.permit(:faculty,:depat_cord)
+  end
 end
