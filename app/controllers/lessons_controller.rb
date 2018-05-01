4f7b7b325b796f4f39457e396bbc31d46988ade2
takehanKosuke <kkoussuke530@icloud.com>
2018-05-01 20:41:25 +0900

    make lessons_controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :are_you_permission?
+  def new
+    
+  end
+
+  def create
+    redirect_to "/users/#{:id}"
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+private
+  def are_you_permission?
+    unless current_user.permission == 1
+      redirect_to "/users/#{:id}", alert:"管理者のみが行えるアクションです"
+    end
+  end
 end
